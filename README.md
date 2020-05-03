d68b58ff9f91ff3231cd9912db4b0a64cd80fc14
Rui Fernandes <rf-91@hotmail.com>
2020-05-03 17:10:18 +0100

    feat: add swagger

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# ASP.NET Core API
-ASP.NET Core Build API training.
+# Quotes API
+A demo app for learning purposes.
+
+![alt text](https://loremipsum.io/assets/images/lorem-ipsum-generator-custom-placeholder-text.jpg "Lorem Ipsum")
+
+## Technologies
+* ASP .NET Core 3.1
+* Entity Framework
--- src/QuotesApi/Startup.cs
+++ src/QuotesApi/Startup.cs
@@ -1,5 +1,7 @@
 namespace QuotesApi
 {
+    using System;
+    using System.Collections.Generic;
     using Microsoft.AspNetCore.Authentication.JwtBearer;
     using Microsoft.AspNetCore.Builder;
     using Microsoft.AspNetCore.Hosting;
@@ -7,6 +9,7 @@ namespace QuotesApi
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using Microsoft.OpenApi.Models;
     using QuotesApi.Data;
 
     public class Startup
@@ -36,8 +39,41 @@ namespace QuotesApi
                 options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
             }).AddJwtBearer(options =>
             {
-                options.Authority = "https://aspnet-core-quotesapi.eu.auth0.com/";
+                options.Authority = "https://aspnet-core-quotesapi.eu.auth0.com";
                 options.Audience = "https://localhost:44326/";
+            });
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo { Title = "Quotes API", Version = "v1" });
+
+                options.AddSecurityDefinition("oauth2", new OpenApiSecurityScheme
+                {
+                    Type = SecuritySchemeType.OAuth2,
+                    Flows = new OpenApiOAuthFlows
+                    {
+                        Password = new OpenApiOAuthFlow
+                        {
+                            TokenUrl = new Uri("https://aspnet-core-quotesapi.eu.auth0.com/oauth/token"),
+                            Scopes = new Dictionary<string, string>()
+                        }
+                    }
+                });
+
+                options.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id = "oauth2",
+                                Type = ReferenceType.SecurityScheme
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
         }
 
@@ -47,6 +83,17 @@ namespace QuotesApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1/swagger.json", "Quotes API");
+
+                options.OAuthClientId("KWEa2Tp2yBdPIpyM1G3w0rPABVkjw9GR");
+                options.OAuthClientSecret("o-ajkYBGTeYrkU3FKlFWJjS1F8Nz0oK4lvV8Ig3ClGgsgVzf7xXWdFWZuI1p8V6m");
+                options.OAuthAppName("Quotes API - Swagger");
+            });
 
             app.UseHttpsRedirection();
 
